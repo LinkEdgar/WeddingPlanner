218d16b30e9a542d56ac933a08303a103f360f37
Reyes <ereyes@M-C02YG123JG5J.local>
2020-03-07 16:55:28 -0500

    Added a GuestFragment to enable users to invite guest via email. Added TODOs for Jimmy

---
+++ WeddingPlanner/app/src/main/java/com/example/weddingplanner/InviteGuestsFragmentViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.weddingplanner
+
+import androidx.lifecycle.ViewModel
+
+class InviteGuestsFragmentViewModel : ViewModel(){
+
+
+    fun validateEmail(email: String):Boolean{
+        //todo validate email
+        return false
+    }
+}
---
+++ WeddingPlanner/app/src/main/java/com/example/weddingplanner/ui/InviteGuestsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.weddingplanner.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.weddingplanner.InviteGuestsFragmentViewModel
+import com.example.weddingplanner.R
+
+class InviteGuestsFragment : Fragment(){
+
+    //todo make this fragment show in one of the hambuger menu screens
+
+    private lateinit var viewModel: InviteGuestsFragmentViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_invite_guests, container, false)
+        //TODO find out why the following method has been deprecated and find its alternative
+        viewModel = ViewModelProviders.of(this).get(InviteGuestsFragmentViewModel::class.java)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
