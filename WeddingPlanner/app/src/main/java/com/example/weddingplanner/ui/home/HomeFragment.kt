369665e9b308189d5c5c46a8863e08086c58e5cf
Reyes <ereyes@M-C02YG123JG5J.local>
2020-03-18 18:34:14 -0400

    Setup initial functionality for reset password button in ForgotPasswordFragment

--- WeddingPlanner/app/src/main/java/com/example/weddingplanner/ui/home/HomeFragment.kt
+++ WeddingPlanner/app/src/main/java/com/example/weddingplanner/ui/home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.weddingplanner.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.example.weddingplanner.Activities.LoginActvitiy
 import com.example.weddingplanner.R
 
 class HomeFragment : Fragment() {
@@ -24,10 +22,6 @@ class HomeFragment : Fragment() {
         setViews(root)
 
         getInviteInfo()
-        //TODO test code
-        val intent = Intent(context, LoginActvitiy::class.java)
-        startActivity(intent)
-
         return root
     }
 
